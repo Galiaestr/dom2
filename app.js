5a772cadd216328e4e71314f537f158217507d86
Galicia Estrada <galiciastrda@gmail.com>
2026-03-12 19:46:55 -0600

    Agregando miniaturas

--- app.js
+++ app.js
@@ -7,3 +7,28 @@ const data = [
   { id: "p06", title: "Ruta", desc: "Camino en perspectiva", src: "https://picsum.photos/id/1005/1200/675" }
 ];
 
+
+const thumbs = document.querySelector("#thumbs"); //miniaturas
+const heroImg = document.querySelector("#heroImg"); //imagen principal
+const heroTitle = document.querySelector("#heroTitle"); //titulo de la imagen
+const heroDesc = document.querySelector("#heroDesc"); //descripción de la imagen
+const counter = document.querySelector("#counter"); //controlador de imagenes
+const likeBtn = document.querySelector("#likeBtn"); //boton de "me gusta"
+
+//trabajar con el estado de la aplicacion
+let currentIndex = 0; //indice de la imagen actual
+const likes ={}; //objeto para almacenar los "me gusta" por imagen
+
+// renderizar las miniaturas
+function renderThumbs() {
+  thumbs.innerHTML = data.map((item, index) => {
+    return `
+      <article class="thumb ${index === currentIndex ? "active" : ""}" data-index="${index}">
+        <span class="badge">${index + 1}</span>
+        <img src="${item.src}" alt="${item.title}" /> 
+      </article>
+    `;
+  }).join("");
+}
+
+renderThumbs(); //llamar a la función para mostrar las miniaturas
